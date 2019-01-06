b0a36e3cacbe2ee6dd45f86e84e4ccf587e71c23
Hiroyuki Miyazaki <heromiya@heromiya.net>
2019-01-07 00:36:25 +0900

    modified:   Makefile 	modified:   main.sh

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
 geonames/$(CNT).zip:
 	wget -q http://download.geonames.org/export/dump/$(CNT).zip -O $@
 geonames/$(CNT).txt: geonames/$(CNT).zip
-	unzip $<
+	unzip -f $< -d geonames
+geonames/$(CNT).sqlite: geonames/$(CNT).txt
+	spatialite $@ "CREATE TABLE $(CNT) (geonameid integer primary key, name varchar(200), asciiname varchar(200), alternatenames varchar(10000), latitude real, longitude real, featureclass char(1), featurecode varchar(10), countrycode char(2), cc2 varchar(200), admin1code varchar(20), admin2code varchar(80), admin3code varchar(20), admin4code varchar(20), population bigint, elevation integer, dem integer, timezone varchar(40), modificationdate char(10));"
+	spatialite -separator '	' $@ ".import $< $(CNT)"
+	spatialite $@ "DELETE FROM $(CNT) WHERE featurecode != 'PPL';"
+	spatialite $@ "SELECT AddGeometryColumn('$(CNT)','geom',4326,'POLYGON',2);"
+	spatialite $@ "UPDATE $(CNT) SET geom = Buffer(MakePoint(longitude, latitude, 4326),$(BUF))"
+	spatialite $@ "DELETE FROM geometry_columns WHERE f_table_name = 'tiles';"
+	spatialite $@ "DROP TABLE IF EXISTS tiles;"
+	spatialite $@ "CREATE TABLE tiles (gid integer primary key AUTOINCREMENT,qkey varchar(64));"
+	spatialite $@ "SELECT AddGeometryColumn('tiles', 'geom' ,4326, 'POLYGON', 'XY');"
+
+ROI/$(CNT).ROI.txt: geonames/$(CNT).sqlite
+	mkdir -p `dirname $@`
+	ogrinfo $< $(CNT) -sql "select MbrMaxX(geom), MbrMaxY(geom), MbrMinX(geom), MbrMinY(geom) from $(CNT)" | grep -e "MbrMinX(geom) (Real)" -e "MbrMaxX(geom) (Real)" -e "MbrMinY(geom) (Real)" -e "MbrMaxY(geom) (Real)" | awk 'BEGIN{OFS="|"}/MaxX/{maxx=$$4}/MaxY/{maxy=$$4}/MinX/{minx=$$4}/MinY/{miny=$$4; print minx,maxx,miny,maxy}' > $@
--- main.sh
+++ main.sh
@@ -13,31 +13,24 @@
 #LATMAX=17.113576
 #LATMIN=15.876399
 
-COUNTY=KE
-BUF=0.025 # approx 3 km
+export CNT=KE
+export BUF=0.025 # approx 3 km
+export DB=geonames/$CNT.sqlite
 
+make $DB ROI/$CNT.ROI.txt
 
-ZLEVEL=15 # Tile size is 1223 x 1223 m
+ZLEVEL=17 # Tile size is 307 x 307 m
 PREREQ="var tilebelt = require('tilebelt');"
-DB=assignmentTiles.sqlite
-
-rm -f $DB
-spatialite $DB <<EOF
-DELETE FROM geometry_columns WHERE f_table_name = 'tiles';
-DROP TABLE IF EXISTS tiles;
-CREATE TABLE tiles (
-        gid integer primary key AUTOINCREMENT,
-        qkey varchar(64)
-);
-SELECT AddGeometryColumn('tiles', 'the_geom' ,4326, 'POLYGON', 'XY');
-EOF
-
-ogrinfo PPL-5km.gpkg -al | grep -e minx -e maxx -e miny -e maxy | grep = | awk 'BEGIN{OFS="|"}/minx/{minx=$4}/maxx/{maxx=$4}/miny/{miny=$4}/maxy/{maxy=$4; print minx,maxx,miny,maxy}' > ROI.txt
 
 IFS='
 '
+mkdir -p SQL
+SQL=SQL/$CNT.insert.sql
+rm -f $SQL
+touch $SQL
 
-for ROI in `cat ROI.txt`; do
+
+for ROI in `cat ROI/$CNT.ROI.txt`; do
 
     LONMIN=`echo $ROI | cut -f 1 -d '|'`
     LONMAX=`echo $ROI | cut -f 2 -d '|'`
@@ -52,12 +45,17 @@ for ROI in `cat ROI.txt`; do
     for XTILE in `seq $XTILEMIN $XTILEMAX`; do
 	for YTILE in `seq $YTILEMIN $YTILEMAX`; do
 	    QKey=`nodejs -e "$PREREQ process.stdout.write(String(tilebelt.tileToQuadkey([$XTILE,$YTILE,$ZLEVEL])))"`
-	    TILELONMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[0]))"`
-	    TILELATMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[1]))"`
-	    TILELONMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[2]))"`
-	    TILELATMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[3]))"`
-	    echo "INSERT INTO tiles (qkey, the_geom) VALUES ('$QKey',ST_GeomFromText('POLYGON (($TILELONMIN $TILELATMIN, $TILELONMIN $TILELATMAX, $TILELONMAX $TILELATMAX, $TILELONMAX $TILELATMIN, $TILELONMIN $TILELATMIN))', 4326));" | spatialite $DB
+	    grep $QKey $SQL
+	    if [ $? -eq 1 ]; then
+		TILELONMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[0]))"`
+		TILELATMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[1]))"`
+		TILELONMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[2]))"`
+		TILELATMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[3]))"`
+		echo "INSERT INTO tiles (qkey, the_geom) VALUES ('$QKey',ST_GeomFromText('POLYGON (($TILELONMIN $TILELATMIN, $TILELONMIN $TILELATMAX, $TILELONMAX $TILELATMAX, $TILELONMAX $TILELATMIN, $TILELONMIN $TILELATMIN))', 4326));" >> $SQL
+	    fi
 	done
     done
 
 done
+
+spatialite $DB < insert.sql
