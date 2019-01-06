abae040180864fc66cecdb56096948b4af1ed20c
Hiroyuki Miyazaki <heromiya@heromiya.net>
2019-01-07 00:45:40 +0900

    added parallel

--- main.sh
+++ main.sh
@@ -19,7 +19,7 @@ export DB=geonames/$CNT.sqlite
 
 make $DB ROI/$CNT.ROI.txt
 
-ZLEVEL=17 # Tile size is 307 x 307 m
+export ZLEVEL=17 # Tile size is 307 x 307 m
 PREREQ="var tilebelt = require('tilebelt');"
 
 IFS='
@@ -29,6 +29,20 @@ SQL=SQL/$CNT.insert.sql
 rm -f $SQL
 touch $SQL
 
+function sql_insert() {
+    XTILE=$1
+    YTILE=$2
+    QKey=`nodejs -e "$PREREQ process.stdout.write(String(tilebelt.tileToQuadkey([$XTILE,$YTILE,$ZLEVEL])))"`
+    grep $QKey $SQL
+    if [ $? -eq 1 ]; then
+	TILELONMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[0]))"`
+	TILELATMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[1]))"`
+	TILELONMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[2]))"`
+	TILELATMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[3]))"`
+	echo "INSERT INTO tiles (qkey, the_geom) VALUES ('$QKey',ST_GeomFromText('POLYGON (($TILELONMIN $TILELATMIN, $TILELONMIN $TILELATMAX, $TILELONMAX $TILELATMAX, $TILELONMAX $TILELATMIN, $TILELONMIN $TILELATMIN))', 4326));" >> $SQL
+    fi
+}
+export -f sql_insert
 
 for ROI in `cat ROI/$CNT.ROI.txt`; do
 
@@ -42,20 +56,7 @@ for ROI in `cat ROI/$CNT.ROI.txt`; do
     XTILEMAX=`nodejs -e "$PREREQ Tile=tilebelt.pointToTile($LONMAX, $LATMAX, $ZLEVEL); process.stdout.write(String(Tile[0]))"`
     YTILEMIN=`nodejs -e "$PREREQ Tile=tilebelt.pointToTile($LONMAX, $LATMAX, $ZLEVEL); process.stdout.write(String(Tile[1]))"`
 
-    for XTILE in `seq $XTILEMIN $XTILEMAX`; do
-	for YTILE in `seq $YTILEMIN $YTILEMAX`; do
-	    QKey=`nodejs -e "$PREREQ process.stdout.write(String(tilebelt.tileToQuadkey([$XTILE,$YTILE,$ZLEVEL])))"`
-	    grep $QKey $SQL
-	    if [ $? -eq 1 ]; then
-		TILELONMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[0]))"`
-		TILELATMIN=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[1]))"`
-		TILELONMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[2]))"`
-		TILELATMAX=`nodejs -e "$PREREQ BB=tilebelt.tileToBBOX([$XTILE,$YTILE,$ZLEVEL]); process.stdout.write(String(BB[3]))"`
-		echo "INSERT INTO tiles (qkey, the_geom) VALUES ('$QKey',ST_GeomFromText('POLYGON (($TILELONMIN $TILELATMIN, $TILELONMIN $TILELATMAX, $TILELONMAX $TILELATMAX, $TILELONMAX $TILELATMIN, $TILELONMIN $TILELATMIN))', 4326));" >> $SQL
-	    fi
-	done
-    done
-
+    parallel --nice 10 --progress sql_insert {} {} ::: `seq $XTILEMIN $XTILEMAX` ::: `seq $YTILEMIN $YTILEMAX`
 done
 
 spatialite $DB < insert.sql
